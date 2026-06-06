1fac1d32875dd5a7b60e15a9057a04b58e39a16b
Võ Dương Khang <114740029+duokhang1676@users.noreply.github.com>
2026-06-06 22:36:12 +0700

    update model ver3

--- models/convert-model.py
+++ models/convert-model.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 
 # Load a YOLO11n PyTorch model
-model = YOLO("yolo11n-ver2.pt")
+model = YOLO("yolo11n-ver3.pt")
 
 # Export the model to TensorRT
 model.export(format="engine")  # creates 'yolo11n.engine'
 
 # Load the exported TensorRT model
-trt_model = YOLO("yolo11n-ver2.engine")
+trt_model = YOLO("yolo11n-ver3.engine")
 
 # Run inference
 # results = trt_model("https://ultralytics.com/images/bus.jpg")
